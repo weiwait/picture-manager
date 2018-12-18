58d5fe3969f29e28af2bb6c8566cb05dd3db4b15
weiwait <lijingwei.weiwait@foxmail.com>
2018-12-18 17:10:56 +0800

    修复user表属性

--- app/Http/Controllers/Api/PassportController.php
+++ app/Http/Controllers/Api/PassportController.php
@@ -26,6 +26,6 @@ class PassportController extends Controller
         $user->updated_at = now();
         $user->save();
 
-        return response()->json(['access_token' => 'Bearer' .  $res]);
+        return response()->json(['access_token' => 'Bearer' .  $accessToken]);
     }
 }
