704101a0d09059d8f90310cd1b73c098ffea73aa
weiwait <lijingwei.weiwait@foxmail.com>
2018-12-18 12:57:13 +0800

    testing miniprogram login

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/login', 'Api/LoginController@login');
+Route::post('login', 'Api\LoginController@login');
+
